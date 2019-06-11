563832541bc81a9349f403ef80663aa365de30dc
Alberto Cuoci <alberto.cuoci@polimi.it>
2019-06-11 16:19:38 +0200

    Fixed bug in ROPA

--- src/Utilities.hpp
+++ src/Utilities.hpp
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------*\
+/*-----------------------------------------------------------------------*\
 |    ___                   ____  __  __  ___  _  _______                  |
 |   / _ \ _ __   ___ _ __ / ___||  \/  |/ _ \| |/ / ____| _     _         |
 |  | | | | '_ \ / _ \ '_ \\___ \| |\/| | | | | ' /|  _| _| |_ _| |_       |
@@ -16,7 +16,7 @@
 |                                                                         |
 |   This file is part of OpenSMOKE++PostProcessor.                        |
 |                                                                         |
-|	License                                                           |
+|	License                                                               |
 |                                                                         |
 |   Copyright(C) 2014, 2013  Alberto Cuoci                                |
 |   OpenSMOKE++PostProcessor is free software: you can redistribute it    |
@@ -37,112 +37,142 @@
 #include "Utilities.h"
 
 void MergePositiveAndNegativeBars
-			(	const std::vector<unsigned int>& positive_indices, 
-				const std::vector<unsigned int>& negative_indices,
-				const std::vector<double>& positive_coefficients, 
-				const std::vector<double>& negative_coefficients, 
-				std::vector<int>& indices,
-				std::vector<double>& coefficients)
+(const std::vector<unsigned int>& positive_indices,
+	const std::vector<unsigned int>& negative_indices,
+	const std::vector<double>& positive_coefficients,
+	const std::vector<double>& negative_coefficients,
+	std::vector<int>& indices,
+	std::vector<double>& coefficients)
 {
 	unsigned int n = positive_indices.size() +
-					 negative_indices.size();
+		negative_indices.size();
+
+	std::vector<int> signum(n);
 
 	indices.resize(n);
 	coefficients.resize(n);
-	for(unsigned int i=0;i<positive_coefficients.size();i++)
+	for (unsigned int i = 0; i < positive_coefficients.size(); i++)
 	{
 		indices[i] = positive_indices[i];
 		coefficients[i] = positive_coefficients[i];
+		signum[i] = 1;
 	}
-	for(unsigned int i=0;i<negative_coefficients.size();i++)
+	for (unsigned int i = 0; i < negative_coefficients.size(); i++)
 	{
-		indices[i+positive_indices.size()] = -negative_indices[i];
-		coefficients[i+positive_indices.size()] = -negative_coefficients[i];
+		indices[i + positive_indices.size()] = -negative_indices[i];
+		coefficients[i + positive_indices.size()] = -negative_coefficients[i];
+		signum[i + positive_indices.size()] = -1;
 	}
-	
+
+	std::vector<double> tmp = coefficients;
+
 	OpenSMOKE_Utilities::ReorderPairsOfVectors(coefficients, indices);
 	std::reverse(indices.begin(), indices.end());
 	std::reverse(coefficients.begin(), coefficients.end());
-	for(unsigned int i=0;i<n;i++)
-		if (indices[i]<0)	
+
+	OpenSMOKE_Utilities::ReorderPairsOfVectors(tmp, signum);
+	std::reverse(signum.begin(), signum.end());
+
+
+	for (unsigned int i = 0; i < n; i++)
+		if (signum[i] == -1)
 		{
 			indices[i] *= -1;
 			coefficients[i] *= -1.;
 		}
-		
+
 }
 
 
-void MergeBars( const std::vector<unsigned int>& total_indices, 
-				const std::vector<double>& total_coefficients, 
-				std::vector<int>& indices,
-				std::vector<double>& coefficients)
+void MergeBars(const std::vector<unsigned int>& total_indices,
+	const std::vector<double>& total_coefficients,
+	std::vector<int>& indices,
+	std::vector<double>& coefficients)
 {
 	unsigned int n = total_indices.size();
 
+	std::vector<int> signum(n);
+
 	indices.resize(n);
 	coefficients.resize(n);
-	for(unsigned int i=0;i<total_indices.size();i++)
+	for (unsigned int i = 0; i < total_indices.size(); i++)
 		if (total_coefficients[i] < 0.)
 		{
 			coefficients[i] = -total_coefficients[i];
 			indices[i] = -total_indices[i];
+			signum[i] = -1;
 		}
 		else
 		{
 			coefficients[i] = total_coefficients[i];
 			indices[i] = total_indices[i];
+			signum[i] = 1;
 		}
+
+	std::vector<double> tmp = coefficients;
 
 	OpenSMOKE_Utilities::ReorderPairsOfVectors(coefficients, indices);
 	std::reverse(indices.begin(), indices.end());
 	std::reverse(coefficients.begin(), coefficients.end());
-	for(unsigned int i=0;i<n;i++)
-		if (indices[i]<0)	
+
+	OpenSMOKE_Utilities::ReorderPairsOfVectors(tmp, signum);
+	std::reverse(signum.begin(), signum.end());
+
+	for (unsigned int i = 0; i < n; i++)
+		if (signum[i] == -1)
 		{
 			indices[i] *= -1;
 			coefficients[i] *= -1.;
 		}
 }
 
-void MergeBars( const std::vector<unsigned int>& total_indices, 
-				const std::vector<double>& total_coefficients, 
-				const std::vector<double>& total_peaks,
-				std::vector<int>& indices,
-				std::vector<double>& coefficients, 
-				std::vector<double>& peaks)
+void MergeBars(const std::vector<unsigned int>& total_indices,
+	const std::vector<double>& total_coefficients,
+	const std::vector<double>& total_peaks,
+	std::vector<int>& indices,
+	std::vector<double>& coefficients,
+	std::vector<double>& peaks)
 {
 	unsigned int n = total_indices.size();
+
+	std::vector<int> signum(n);
 
 	indices.resize(n);
 	coefficients.resize(n);
 	peaks.resize(n);
-	for(unsigned int i=0;i<total_indices.size();i++)
+	for (unsigned int i = 0; i < total_indices.size(); i++)
 		if (total_coefficients[i] < 0.)
 		{
 			coefficients[i] = -total_coefficients[i];
 			indices[i] = -total_indices[i];
 			peaks[i] = total_peaks[i];
+			signum[i] = -1;
 		}
 		else
 		{
 			coefficients[i] = total_coefficients[i];
 			indices[i] = total_indices[i];
 			peaks[i] = total_peaks[i];
+			signum[i] = 1;
 		}
 
 	std::vector<double> tmp = coefficients;
+	std::vector<double> tmp1 = coefficients;
+
 	OpenSMOKE_Utilities::ReorderPairsOfVectors(coefficients, indices);
 	std::reverse(indices.begin(), indices.end());
 	std::reverse(coefficients.begin(), coefficients.end());
-	for(unsigned int i=0;i<n;i++)
-		if (indices[i]<0)	
+
+	OpenSMOKE_Utilities::ReorderPairsOfVectors(tmp, signum);
+	std::reverse(signum.begin(), signum.end());
+
+	for (unsigned int i = 0; i < n; i++)
+		if (signum[i] == -1)
 		{
 			indices[i] *= -1;
 			coefficients[i] *= -1.;
 		}
 
-	OpenSMOKE_Utilities::ReorderPairsOfVectors(tmp, peaks);
+	OpenSMOKE_Utilities::ReorderPairsOfVectors(tmp1, peaks);
 	std::reverse(peaks.begin(), peaks.end());
 }
-
